04c29797ed18fca1774feed0109abc55f67a830b
Scott Sansame <ssansam@hotmail.com>
2010-09-23 16:21:24 -0700

    Added Finishing Touches to the project.
    Signed-off-by: Scott Sansame <ssansam@hotmail.com>

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -1,6 +1,6 @@
 /**
 @author Scott Sansam
-@version 0.2
+@version 0.4
 
 A simple hello world program, created by a group.
 */
@@ -9,13 +9,13 @@ public class HelloWorldMain {
 
 	public static void main(String[] args) {
 		
-	
+System.out.println("Hello, Team!" + "\n" + "My name is Scott Sansam");	
 JamesDickes james = new JamesDickes();
 System.out.println("Welcome " + james.toString() + "!");
 AlfredVasquez alfred = new AlfredVasquez();
 System.out.println("Welcome " + alfred.toString() + "!");
-//LuisCorral Luis = new LuisCorral();
-//System.out.println("Welcome " + luis.toString() + "!");
+LuisCorral luis = new LuisCorral();
+System.out.println("Welcome " + luis.toString() + "!");
 
 
 	}
